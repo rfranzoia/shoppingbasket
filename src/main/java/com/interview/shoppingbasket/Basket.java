3bf1703e95f9601c18dce98e2ad70b1d8669d385
Romeu Franzoia Junior <rfranzoia@gmail.com>
2023-02-24 16:53:33 +0000

    implemented consolidate method for basket

--- src/main/java/com/interview/shoppingbasket/Basket.java
+++ src/main/java/com/interview/shoppingbasket/Basket.java
@@ -22,6 +22,29 @@ public class Basket {
     }
 
     public void consolidateItems() {
-        // Exercise - implement this function
+        if (items.isEmpty()) return;
+
+        // group all product codes
+        Map<String, Integer> consolidatedItemsMap =
+                items.stream()
+                        .collect(
+                                Collectors.groupingBy(BasketItem::getProductCode,
+                                        Collectors.reducing(0, BasketItem::getQuantity, Integer::sum)));
+
+        // rebuild the list of basket items
+        List<BasketItem> consolidatedItems = consolidatedItemsMap.entrySet().stream()
+                .map(e -> {
+                    BasketItem bI = new BasketItem();
+                    bI.setProductCode(e.getKey());
+                    bI.setQuantity(e.getValue());
+                    bI.setProductName(items.stream()
+                            .filter(i -> i.getProductCode() == e.getKey())
+                            .findFirst().get().getProductName());
+                    return bI;
+                }).collect(Collectors.toList());
+
+        // move to original list
+        items.clear();
+        items.addAll(consolidatedItems);
     }
 }
